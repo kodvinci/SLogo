2d5a9d962e424ec525e82c2c07b55b16e5c22401
arcueidyang <arcueidyang@gmail.com>
2013-02-21 21:09:05 -0500

    made some methods, and added some old classes
    my need further modifications

---
+++ src/movements/Movements.java
@@ -0,0 +1,38 @@
+package movements;
+
+import util.*;
+
+
+public class Movements {
+   
+    
+    
+    public static void moveForward(Sprite animal){
+       
+   }
+    public static void moveForward(Location myPosition,double myAngle, double distance){
+        
+    }
+    public static void moveBackward(Sprite animal){
+        
+    }
+    public static void moveBackward(Location myPosition,double myAngle, double distance){
+         
+    }
+    public static void turnRight(double myAngle , double change){
+        
+    }
+    public static void turnRight(Sprite animal , double change){
+        
+    }
+    public static void turnLeft(double myAngle , double change){
+        
+    }
+    public static void turnLeft(Sprite animal , double change){
+        
+    }
+    
+    
+    
+    
+}
