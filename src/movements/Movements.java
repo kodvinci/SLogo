9d4935a6e7614b9f1299f199f1b2a9e3e487251a
arcueidyang <arcueidyang@gmail.com>
2013-02-22 16:05:08 -0500

    cut duplicated code

--- src/movements/Movements.java
+++ src/movements/Movements.java
@@ -7,30 +7,20 @@ public class Movements {
    
     
     
-    public static void moveForward(Sprite animal){
+    public static void moveForward(Sprite animal,double distance){
        
-   }
+    }
     public static void moveForward(Location myPosition,double myAngle, double distance){
         
     }
-    public static void moveBackward(Sprite animal){
-        
-    }
-    public static void moveBackward(Location myPosition,double myAngle, double distance){
-         
-    }
+    
     public static void turnRight(double myAngle , double change){
         
     }
     public static void turnRight(Sprite animal , double change){
         
     }
-    public static void turnLeft(double myAngle , double change){
-        
-    }
-    public static void turnLeft(Sprite animal , double change){
-        
-    }
+    
     
     
     
