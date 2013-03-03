4f0352a6f53d021c40f1b38bfa817a1f3867d971
Jerry <th365thli@gmail.com>
2013-03-03 16:48:09 -0500

    fixing turtle

--- src/exceptions/SyntaxException.java
+++ src/exceptions/SyntaxException.java
@@ -13,11 +13,18 @@ public class SyntaxException extends Exception {
      * set serial version ID
      */
     private static final long serialVersionUID = 1L;
-
+    
+    /**
+     * Syntax exception
+     */
     public SyntaxException () {
         super();
     }
-
+    
+    /**
+     * Syntax exception with message
+     * @param message   the string message
+     */
     public SyntaxException (String message) {
         super(message);
     }
