d6a354f393e85b9d72ad345a533b1a5c65aeb373
Jerry Li <th365thli@gmail.com>
2013-03-25 21:07:47 -0400

    checksytle

--- src/slogo/Model.java
+++ src/slogo/Model.java
@@ -8,7 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import object.Turtle;
-import view.DisplayArea;
 
 
 /**
@@ -77,11 +76,20 @@ public class Model {
     public Controller getController () {
         return myController;
     }
-
+    
+    /**
+     * add user defined commands
+     * @param string    String
+     * @param command   commands
+     */
     public void addUserCommands (String string, ICommand command) {
         myUserToCommands.put(string, command);
     }
-
+    
+    /**
+     * return user command map
+     * @return
+     */
     public Map<String, ICommand> getUserCommands () {
         return myUserToCommands;
     }
@@ -95,7 +103,10 @@ public class Model {
     public Turtle getMyTurtle (int index) {
         return myTurtles.get(index);
     }
-
+    
+    /**
+     * adds a new turtle
+     */
     public void addNewTurtle () {
         Turtle turtle = new Turtle();
         myTurtles.put(myTurtleNumber, turtle);
@@ -150,11 +161,20 @@ public class Model {
         if (!myVariables.containsKey(name)) { throw new NoSuchVariableException(); }
         return Double.parseDouble(myVariables.get(name));
     }
-
+    
+    /**
+     * set variable value
+     * @param key       string key
+     * @param value     value
+     */
     public void setVariableValue (String key, Double value) {
         myVariables.put(key, value + "");
     }
-
+    
+    /**
+     * get usre variables
+     * @return
+     */
     public Map<String, String> getUserVariables () {
         return myVariables;
     }
@@ -165,29 +185,50 @@ public class Model {
     public void clearVariable () {
         myVariables.clear();
     }
-
+    
+    /**
+     * clear future activiated turtles
+     */
     public void clearFutureActivatedTurtles () {
         myFutureActivatedTurtles.clear();
     }
-
+    
+    /**
+     * activated turtles
+     */
     public void activatedAllTurtles () {
         for (int i = 0; i < myTurtleNumber; i++) {
             myFutureActivatedTurtles.add(i);
         }
     }
-
+    
+    /**
+     * add future activated turtles
+     * @param turtleIndex       turtle index
+     */
     public void addFutureActivatedTurtles (int turtleIndex) {
         myFutureActivatedTurtles.add(turtleIndex);
     }
-
+    
+    /**
+     * get current activated turtles
+     * @return
+     */
     public List<Integer> getMyCurrentActivatedTurtles () {
         return myCurrentActivatedTurtles;
     }
-
+    
+    /**
+     * replace activated turltes
+     */
     public void replaceActivatedTurtles () {
         myCurrentActivatedTurtles = new ArrayList<Integer>(myFutureActivatedTurtles);
     }
-
+    
+    /**
+     * add current activated turtles
+     * @param turtleIndex turlte index
+     */
     public void addCurrentActivatedTurtles (int turtleIndex) {
         myCurrentActivatedTurtles.add(turtleIndex);
     }
